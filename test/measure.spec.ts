ddda5da8d63f99c9460db7e5b819dfdb8cd73d28
RafaelGSS <rafael.nunu@hotmail.com>
2020-07-07 00:08:12 -0300

    chore(ci): add test ci with measure test

--- test/measure.spec.ts
+++ test/measure.spec.ts
@@ -1,12 +1,77 @@
-import { Measure } from '..';
+import { Measure } from '../index';
 
 describe('Measure', () => {
-  let measure: any;
-  beforeEach(() => {
-    measure = Measure();
+  describe('no args', () => {
+    let descriptor: any;
+    let measure: any;
+
+    beforeEach(() => {
+      descriptor = { value: () => new Number(1) };
+      measure = Measure();
+      measure({}, 'example key', descriptor);
+    });
+
+    it('should return function', () => {
+      expect(measure).toBeInstanceOf(Function);
+    });
+
+    it('should replace the descriptor.value with sync function', () => {
+      expect(descriptor.value).toBeInstanceOf(Function);
+      expect(descriptor.value()).toBeInstanceOf(Number);
+    });
   });
 
-  it('epext', () => {
-    expect(measure).toBeInstanceOf(Function);
-  })
+  describe('with async: true', () => {
+    let descriptor: any;
+    let measure: any;
+
+    beforeEach(() => {
+      descriptor = { value: () => new Number(1) };
+      measure = Measure({ asyncFunction: true });
+      measure({}, 'example key', descriptor);
+    });
+
+    it('should return function', () => {
+      expect(measure).toBeInstanceOf(Function);
+    });
+
+    it('should replace the descriptor.value with async function', () => {
+      expect(descriptor.value).toBeInstanceOf(Function);
+      expect(descriptor.value()).toBeInstanceOf(Promise);
+    });
+  });
+
+  describe('with callback use', () => {
+    let descriptor: any;
+    let measure: any;
+    let callback: any;
+
+    beforeEach(() => {
+      descriptor = { value: () => new Number(1) };
+      callback = jest.fn();
+      measure = Measure({ cb: callback });
+      measure({}, 'example key', descriptor);
+      descriptor.value();
+    });
+
+    it('should return function', () => {
+      expect(measure).toBeInstanceOf(Function);
+    });
+
+    it('should replace the descriptor.value with sync function', () => {
+      expect(descriptor.value).toBeInstanceOf(Function);
+      expect(descriptor.value()).toBeInstanceOf(Number);
+    });
+
+    it('should call the callback', () => {
+      expect(callback).toHaveBeenCalledWith([
+        expect.objectContaining({
+          duration: expect.any(Number),
+          entryType: 'measure',
+          name: 'example key',
+          startTime: expect.any(Number),
+        }),
+      ]);
+    });
+  });
 });
